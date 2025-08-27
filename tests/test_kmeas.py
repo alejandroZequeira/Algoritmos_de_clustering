a4ee24451b79f9f807db52570427f25c51faec00
alejandroZequeira <alejandrozequeirarosa@gmail.com>
2025-08-26 20:47:12 -0600

    create objects for axo

---
+++ tests/test_kmeas.py
@@ -0,0 +1,47 @@
+from axo.contextmanager import AxoContextManager
+from axo.endpoint.manager import DistributedEndpointManager 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from Algoritmos.clustering_axo import KMeansAnalyzer
+import asyncio
+
+def dem():
+    dem = DistributedEndpointManager()
+    dem.add_endpoint(
+        endpoint_id  = "axo-endpoint-0",
+        hostname     = "localhost",
+        protocol     = "tcp",
+        req_res_port = 16667,
+        pubsub_port  = 16666
+    )
+    return dem
+async def test_create_kmeas():
+    np.random.seed(42)
+    data = pd.DataFrame({
+        "X": np.random.rand(30) * 10,
+        "Y": np.random.rand(30) * 10
+    })
+
+    with AxoContextManager.distributed(endpoint_manager=dem()) as dcm:
+        kmeans_model:KMeansAnalyzer = KMeansAnalyzer(data=data)
+        kmeans_model.mostrar_columnas()
+        _ = await kmeans_model.persistify()
+        result = kmeans_model.aplicar_kmeans(columnas=["X", "Y"], n_clusters=3)
+        print(result)
+        
+        kmeans_model.ver_datos()
+
+        # plt.figure(figsize=(8,6))
+        # for cluster in range(3):
+        #     cluster_data = data[labels == cluster]
+        #     plt.scatter(cluster_data["X"], cluster_data["Y"], label=f"Cluster {cluster}")
+
+        # plt.xlabel("X")
+        # plt.ylabel("Y")
+        # plt.title("Clusters KMeans")
+        # plt.legend()
+        # plt.show()
+        
+if __name__=="__main__":
+    asyncio.run(test_create_kmeas())
